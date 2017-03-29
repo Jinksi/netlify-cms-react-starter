383a3f90a527f33cf1fa14ee3bb1bfe297da0d73
Jinksi <ericjinks@gmail.com>
2017-03-29 12:01:55 +1000

    Add <Flex />

--- src/components/common.js
+++ src/components/common.js
@@ -1,11 +1,5 @@
 import styled from 'styled-components'
 
-export const Container = styled.div`
-  margin: 0 auto;
-  max-width: 1111px;
-  width: 90vw;
-`
-
 export const Section = styled.section`
   width: 100%;
   padding: ${props => {
@@ -13,4 +7,30 @@ export const Section = styled.section`
     if (props.thin) return '2.5rem 0'
     return '5rem 0'
   }};
+`
+
+export const Container = styled.div`
+  margin: 0 auto;
+  max-width: 1111px;
+  width: 90vw;
+`
+
+export const Flex = styled.div`
+  display: flex;
+  flex-direction: ${props => props.column ? 'column' : 'row'};
+  justify-content: ${props => {
+    if (props.justifyCenter) return 'center'
+    if (props.justifyEnd) return 'flex-end'
+    if (props.justifyBetween) return 'space-between'
+    if (props.justifyAround) return 'space-around'
+    return 'flex-start'
+  }};
+  align-items: ${props => {
+    if (props.alignStart) return 'flex-start'
+    if (props.alignEnd) return 'flex-end'
+    if (props.alignStretch) return 'stretch'
+    return 'center'
+  }};
+  height: ${props => props.fill ? '100%' : 'auto'};
+  width: ${props => props.fill ? '100%' : 'auto'};
 `
