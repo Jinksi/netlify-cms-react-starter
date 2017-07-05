529afb9e68fde2ad23a316795a772893f3442686
Jinksi <ericjinks@gmail.com>
2017-07-05 18:58:50 +1000

    404.html

--- src/views/NoMatch.js
+++ src/views/NoMatch.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 import Page from '../components/Page'
+import PageHeader from '../components/PageHeader'
 import { Section, Container } from '../components/common'
 
 export default () => (
   <Page>
+    <PageHeader title='404' subtitle='<NoMatch />' />
     <Section thin>
       <Container>
-        <h1>404 – Page Not Found ⛈</h1>
+        <h1>404 – Page Not Found ⛈</h1>
       </Container>
     </Section>
     <Helmet>
-      <title>404 – Page Not Found</title>
+      <title>404 – Page Not Found</title>
     </Helmet>
   </Page>
 )
