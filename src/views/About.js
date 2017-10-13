2caf96afbf118fb6c7e925b2e48ec5bf608667f8
Jinksi <ericjinks@gmail.com>
2017-10-13 23:01:30 +1000

    Integrate extra content and fields

--- src/views/About.js
+++ src/views/About.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Helmet from 'react-helmet'
+import Marked from 'react-markdown'
+
 import Page from '../components/Page'
 import PageHeader from '../components/PageHeader'
 import { Container, Section } from '../components/common'
@@ -12,14 +14,12 @@ export default ({ page }) => (
     <PageHeader title={page.title} subtitle={page.subtitle} />
     <Section thin>
       <Container>
-        <h1>Hello World!</h1>
-        <p>A sem vel nec sodales mi vivamus senectus sed potenti a parturient nascetur tincidunt nisi pulvinar rhoncus a. Risus imperdiet taciti suspendisse facilisi a per metus cubilia varius a nostra adipiscing amet ultrices quisque ac mi a. Dictumst a ultrices mi a dignissim ad fermentum eget a nam et a blandit scelerisque. Taciti lorem tempor quam vestibulum dis habitasse vestibulum diam vel est ut proin dis auctor. Suscipit scelerisque orci magna interdum vel bibendum duis netus a consectetur dui magnis ac aliquet sem posuere tincidunt vestibulum.</p>
+        <Marked source={page.section1} />
       </Container>
     </Section>
     <Section thin>
       <Container taCenter skinny>
-        <h1>This is a skinny center-aligned {'<Container />'}</h1>
-        <p>A sem vel nec sodales mi vivamus senectus sed potenti a parturient nascetur tincidunt nisi pulvinar rhoncus a. Risus imperdiet taciti suspendisse facilisi a per metus cubilia varius a nostra adipiscing amet ultrices quisque ac mi a. Dictumst a ultrices mi a dignissim ad fermentum eget a nam et a blandit scelerisque. Taciti lorem tempor quam vestibulum dis habitasse vestibulum diam vel est ut proin dis auctor. Suscipit scelerisque orci magna interdum vel bibendum duis netus a consectetur dui magnis ac aliquet sem posuere tincidunt vestibulum.</p>
+        <Marked source={page.section2} />
       </Container>
     </Section>
   </Page>
--- src/views/Contact.js
+++ src/views/Contact.js
@@ -6,24 +6,12 @@ import NetlifyForm from '../components/NetlifyForm'
 import { Container, Section } from '../components/common'
 import Marked from 'react-markdown'
 
-const content = `
-  # Example contact form
-
-  This form is setup to use Netlify's form handling:
-
-  - the form action is set to the current absolute url: \`action: '/contact/'\`
-  - a name attribute is sent with the form's data \`'form-name': 'Contact'\`
-  - netlify data attributes are added to the form \`data-netlify data-netlify-honeypot\`
-
-  Find out more in the [Netlify Docs](https://www.netlify.com/docs/form-handling/).
-`
-
 export default ({ page }) => (
   <Page>
     <PageHeader title={page.title} subtitle='<Contact />' />
     <Section thin>
       <Container>
-        <Marked source={content} />
+        <Marked source={page.body} />
         <br />
         <h3>{'<NetlifyForm />'}</h3>
         <NetlifyForm />
