7a6d9dd223383ca77187d97f2930ee7113b81474
Jinksi <ericjinks@gmail.com>
2017-10-14 11:26:26 +1000

    Improve netlifyIdentity process

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { render } from 'react-snapshot'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
+import './netlifyIdentity'
 
 const rootEl = document.getElementById('root')
 render(<App />, rootEl)
