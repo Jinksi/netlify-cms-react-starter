feac99e06752d1026671943958bfd057b5b36067
Jinksi <ericjinks@gmail.com>
2017-11-12 12:18:04 +1000

    update parse-content && config.yml

--- functions/parse-content.js
+++ functions/parse-content.js
@@ -9,6 +9,7 @@ const util = require('util')
 const glob = util.promisify(globCb)
 const readFile = util.promisify(fs.readFile)
 const matter = require('gray-matter')
+const yaml = require('js-yaml')
 
 const options = {
   contentDir: './content/',
@@ -17,7 +18,9 @@ const options = {
 
 const getCollectionType = filePath => {
   const pathParsed = path.parse(filePath)
-  const objectKey = pathParsed.dir.replace(options.contentDir, '').replace(/\//g, '.')
+  const objectKey = pathParsed.dir
+    .replace(options.contentDir, '')
+    .replace(/\//g, '.')
   return `${objectKey}`
 }
 
@@ -33,33 +36,45 @@ const getDocumentExt = filePath => {
 
 const parseMarkdown = data => {
   data = matter(data)
-  data = {...data, ...data.data}
+  data = { ...data, ...data.data }
   delete data.data
   return JSON.stringify(data)
 }
 
+const parseYaml = data => {
+  try {
+    data = yaml.safeLoad(data, 'utf8')
+    return JSON.stringify(data)
+  } catch (e) {
+    return console.log(e)
+  }
+}
+
 const getFileContents = filePath => {
-  return readFile(filePath, 'utf8')
-    .then(data => {
-      if (getDocumentExt(filePath) === '.md') {
-        data = parseMarkdown(data)
-      }
-      let documentData = JSON.parse(data)
-      documentData.name = getDocumentName(filePath)
-      let obj = {}
-      _set(obj, getCollectionType(filePath), [documentData])
-      console.log(`✨  Processed ${filePath}`)
-      return obj
-    })
+  return readFile(filePath, 'utf8').then(data => {
+    if (getDocumentExt(filePath) === '.md') {
+      data = parseMarkdown(data)
+    }
+    if (['.yaml', '.yml'].includes(getDocumentExt(filePath))) {
+      data = parseYaml(data)
+    }
+    let documentData = JSON.parse(data)
+    documentData.name = getDocumentName(filePath)
+    let obj = {}
+    _set(obj, getCollectionType(filePath), [documentData])
+    console.log(`✨  Processed ${filePath}`)
+    return obj
+  })
 }
 
 const readFiles = async paths => Promise.all(paths.map(getFileContents))
 
 const combineJSON = async () => {
   // mergeCustomiser concats arrays items
-  const mergeCustomiser = (objValue, srcValue) => _isArray(objValue) ? objValue.concat(srcValue) : objValue
+  const mergeCustomiser = (objValue, srcValue) =>
+    _isArray(objValue) ? objValue.concat(srcValue) : objValue
   console.log(`✨  Reading JSON files in ${options.contentDir}`)
-  const paths = await glob(`${options.contentDir}/**/**.+(json|md)`)
+  const paths = await glob(`${options.contentDir}/**/**.+(json|md|yaml|yml)`)
   const results = await readFiles(paths)
   const data = _mergeWith({}, ...results, mergeCustomiser)
   return JSON.stringify(data, null, 2)
