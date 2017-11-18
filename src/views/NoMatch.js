aeeccd8fca8c4e523b8ce6798df1855e777bec5c
Jinksi <ericjinks@gmail.com>
2017-11-18 14:15:20 +1000

    Remove react-kawaii

--- src/views/NoMatch.js
+++ src/views/NoMatch.js
@@ -1,24 +1,14 @@
 import React from 'react'
 import Helmet from 'react-helmet'
-import { KawaiiPlanet } from 'react-kawaii'
 import Page from '../components/Page'
 import PageHeader from '../components/PageHeader'
-import { Section, Container, Flex } from '../components/common'
-import { color } from '../globalStyles'
+import { Section, Container } from '../components/common'
 
 export default () => (
   <Page>
     <PageHeader title='404' subtitle='<NoMatch />' />
     <Section>
       <Container taCenter>
-        <Flex justifyCenter>
-          <KawaiiPlanet
-            size={200}
-            mood='sad'
-            color={color.primary}
-            text={`😖`}
-          />
-        </Flex>
         <h3>404 – Page Not Found</h3>
       </Container>
     </Section>
