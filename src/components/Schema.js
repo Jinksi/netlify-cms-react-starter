2c52850d63e0be4120b86424c7e821b271808705
Eric Jinks <ericjinks@gmail.com>
2018-02-01 09:28:52 +1000

    Add Schema Component

---
+++ src/components/Schema.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Schema = ({
+  name,
+  address,
+  email,
+  phone: telephone,
+  url,
+  logoUrl,
+  type,
+  openingHours
+}) => {
+  // see http://schema.org/docs/schemas.html
+
+  const data = {
+    '@context': 'http://schema.org/',
+    '@type': type,
+    address: {
+      '@type': 'PostalAddress',
+      streetAddress: address
+    },
+    name,
+    email,
+    telephone,
+    url,
+    openingHours
+  }
+
+  return (
+    <script
+      type='application/ld+json'
+      dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
+    />
+  )
+}
+
+Schema.propTypes = {
+  type: PropTypes.string, // schema type e.g. LocalBusiness
+  name: PropTypes.string,
+  url: PropTypes.string,
+  address: PropTypes.string,
+  email: PropTypes.string,
+  phone: PropTypes.string,
+  logoUrl: PropTypes.string
+}
+
+export default Schema
