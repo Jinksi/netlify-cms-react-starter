d63850822be0198b39ccff5da60269737b63ad76
Eric Jinks <ericjinks@gmail.com>
2018-02-18 12:51:30 +1000

    Update AOS defaults

--- src/components/AOS.js
+++ src/components/AOS.js
@@ -7,9 +7,10 @@ export default ({ options }) => {
   // See more: https://github.com/michalsnik/aos
   const defaultOptions = {
     offset: 300,
-    duration: 400,
+    duration: 300,
     easing: 'ease-out-sine',
-    delay: 0
+    delay: 0,
+    once: true
   }
 
   AOS.init({ ...defaultOptions, ...options })
