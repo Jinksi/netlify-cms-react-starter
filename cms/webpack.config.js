536cb945a572b0d2dcb73ae1972d3fc76400a569
Eric Jinks <ericjinks@gmail.com>
2018-05-16 12:18:51 +1000

    Update cms webpack config, reduce log verbosity

--- cms/webpack.config.js
+++ cms/webpack.config.js
@@ -1,13 +1,20 @@
 // This webpack config is used to compile the JS for the CMS
 const path = require('path')
+const webpack = require('webpack')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 const production = process.env.NODE_ENV === 'production'
 
 console.log(`Building CMS in ${production ? 'production' : 'development'} mode`)
 
-const productionPlugins = production ? [new UglifyJsPlugin()] : []
+const productionPlugins = production
+  ? [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  ]
+  : []
 
 module.exports = {
   entry: './cms.js',
@@ -15,6 +22,7 @@ module.exports = {
     filename: 'cms.bundle.js',
     path: path.resolve(__dirname, '../public/admin/')
   },
+  stats: { warnings: false, children: false },
   module: {
     rules: [
       {
