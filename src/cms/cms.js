a5728d62268bbb9790fb99b9090607b392eee744
Eric Jinks <ericjinks@gmail.com>
2018-07-10 15:24:57 +1000

    Fix import order

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import CMS from 'netlify-cms'
 
+import { HomePageTemplate } from '../templates/HomePage'
+
 if (process.env.NETLIFY_SITE_URL && typeof window !== 'undefined') {
   window.localStorage.setItem('netlifySiteURL', process.env.NETLIFY_SITE_URL)
 }
-
-import { HomePageTemplate } from '../templates/HomePage'
 
 CMS.registerPreviewStyle('/styles.css')
 CMS.registerPreviewTemplate('home-page', ({ entry }) => <HomePageTemplate />)
