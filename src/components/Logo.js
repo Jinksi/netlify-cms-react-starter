38a1ab3f1f64b8e1fd829e6f00048d5e078d8955
Eric Jinks <ericjinks@gmail.com>
2018-07-17 17:10:05 +1000

    Remove src/images

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,13 +1,12 @@
 import React from 'react'
 
-import logo from '../images/logo.svg'
 import './Logo.css'
 
 export default ({ inverted }) => (
   <div
     className="Logo"
     style={{
-      backgroundImage: `url(${logo})`
+      backgroundImage: `url(/images/logo.svg)`
     }}
   />
 )
