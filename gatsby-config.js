3def4735f208b4c2eb9d6c8d57a5391dddb16f6c
Eric Jinks <ericjinks@gmail.com>
2018-07-17 17:24:41 +1000

    Update static images dir in config

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,14 +13,7 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
-        path: `${__dirname}/static/images/uploads`,
-        name: 'uploads'
-      }
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/static/images`,
         name: 'images'
       }
     },
