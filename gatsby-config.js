8bb1dfeb4b5c0ce93b4adc1846ee06b21b8a000e
Eric Jinks <ericjinks@gmail.com>
2018-07-18 12:22:29 +1000

    CMS init

--- gatsby-config.js
+++ gatsby-config.js
@@ -72,7 +72,9 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
-        modulePath: `${__dirname}/src/cms/cms.js`
+        modulePath: `${__dirname}/src/cms/cms.js`,
+        stylesPath: `${__dirname}/src/cms/admin.css`,
+        enableIdentityWidget: true
       }
     },
     'gatsby-plugin-netlify' // make sure to keep it last in the array
--- src/cms/cms.js
+++ src/cms/cms.js
@@ -7,5 +7,15 @@ if (process.env.NETLIFY_SITE_URL && typeof window !== 'undefined') {
   window.localStorage.setItem('netlifySiteURL', process.env.NETLIFY_SITE_URL)
 }
 
-CMS.registerPreviewStyle('/styles.css')
 CMS.registerPreviewTemplate('home-page', ({ entry }) => <HomePageTemplate />)
+
+// Log netlifySiteURL if editing on localhost
+if (
+  window.location.hostname === 'localhost' &&
+  window.localStorage.getItem('netlifySiteURL')
+) {
+  console.log(
+    `%cnetlifySiteURL: ${window.localStorage.getItem('netlifySiteURL')}`,
+    'color: hotpink; font-size: 15px'
+  )
+}
