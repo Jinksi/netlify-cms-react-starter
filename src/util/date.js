a805c6005dfc5f43fe785b205ca4938b4b99e796
Jinksi <ericjinks@gmail.com>
2019-09-14 17:19:37 +1000

    Fix date format

--- src/util/date.js
+++ src/util/date.js
@@ -1,2 +1,4 @@
-import _format from 'date-fns/format'
-export const dateFormatted = date => _format(date, 'MM.DD.YYYY')
+import formatDate from 'date-fns/format'
+import parseISO from 'date-fns/parseISO'
+export const dateFormatted = date =>
+  date && formatDate(parseISO(date), 'MMMM do, yyyy')
--- src/views/SinglePost.js
+++ src/views/SinglePost.js
@@ -10,44 +10,44 @@ import './SinglePost.css'
 export default ({ fields, nextPostURL, prevPostURL }) => {
   const { title, date, postFeaturedImage, body, categories = [] } = fields
   return (
-    <article className='SinglePost section light'>
+    <article className="SinglePost section light">
       {postFeaturedImage && (
         <BackgroundImage
-          className='SinglePost--BackgroundImage'
+          className="SinglePost--BackgroundImage"
           src={postFeaturedImage}
           alt={title}
         />
       )}
 
-      <div className='container skinny'>
-        <Link className='SinglePost--BackButton' to='/blog/'>
+      <div className="container skinny">
+        <Link className="SinglePost--BackButton" to="/blog/">
           <ChevronLeft /> BACK
         </Link>
-        <div className='SinglePost--Content relative'>
-          <div className='SinglePost--Meta'>
+        <div className="SinglePost--Content relative">
+          <div className="SinglePost--Meta">
             {!!categories.length &&
               categories.map(obj => (
-                <span key={obj.category} className='SinglePost--Meta--Category'>
+                <span key={obj.category} className="SinglePost--Meta--Category">
                   {obj.category}
                 </span>
               ))}
             {date && (
-              <span className='SinglePost--Meta--Date'>
+              <span className="SinglePost--Meta--Date">
                 {dateFormatted(date)}
               </span>
             )}
           </div>
 
-          {title && <h1 className='SinglePost--Title'>{title}</h1>}
+          {title && <h1 className="SinglePost--Title">{title}</h1>}
 
-          <div className='SinglePost--InnerContent'>
+          <div className="SinglePost--InnerContent">
             <Content source={body} />
           </div>
 
-          <div className='SinglePost--Pagination'>
+          <div className="SinglePost--Pagination">
             {prevPostURL && (
               <Link
-                className='SinglePost--Pagination--Link prev'
+                className="SinglePost--Pagination--Link prev"
                 to={prevPostURL}
               >
                 Previous Post
@@ -55,7 +55,7 @@ export default ({ fields, nextPostURL, prevPostURL }) => {
             )}
             {nextPostURL && (
               <Link
-                className='SinglePost--Pagination--Link next'
+                className="SinglePost--Pagination--Link next"
                 to={nextPostURL}
               >
                 Next Post
